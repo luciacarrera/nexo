8be6d7f2f282050ec958f258dcaefe6d44508438
Lucia.Carrera-Saenz <lcarrera@uvm.edu>
2022-05-16 11:50:20 -0400

    Created ui base

---
+++ nexo_ui/nexo_ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nexo_ui
+//
+//  Created by Lucía on 5/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nexo_ui/nexo_ui/nexo_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexo_uiApp.swift
+//  nexo_ui
+//
+//  Created by Lucía on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct nexo_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
