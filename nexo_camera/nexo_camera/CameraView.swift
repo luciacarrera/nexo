cec250f25b426e5f024b57fc778b59cb69787299
Lucia.Carrera-Saenz <lcarrera@uvm.edu>
2022-05-16 14:23:01 -0400

    we get the image and basic settings

---
+++ nexo_camera/nexo_camera/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  nexo_camera
+//
+//  Created by Lucía on 5/16/22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        CameraController()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
---
+++ nexo_camera/nexo_camera/nexo_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexo_cameraApp.swift
+//  nexo_camera
+//
+//  Created by Lucía on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct nexo_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
